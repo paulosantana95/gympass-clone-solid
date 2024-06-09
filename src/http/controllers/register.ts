aabc6ddb7e55b0d572a66c700b18ae963e9e3992
Paulo Santana <paulosantana.sa@gmail.com>
2024-06-09 01:45:42 -0300

    refactor: Use dependency inverse for users repository in register.ts

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,5 @@
-import { prisma } from '@/lib/prisma'
-import { registerUseCase } from '@/use-cases/register'
-import { hash } from 'bcryptjs'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/use-cases/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -14,7 +13,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({ name, email, password })
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({ name, email, password })
   } catch (err) {
     return reply.status(409).send()
   }
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma"
+import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
 
 interface registerUseCaseRequest {
@@ -7,24 +8,23 @@ interface registerUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({name, email, password}: registerUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if(userWithSameEmail) {
-    throw new Error("E-mail already exists.")
-  }
-
-  await prisma.user.create({
-    data: {
+  async execute({name, email, password}: registerUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+  
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+  
+    if(userWithSameEmail) {
+      throw new Error("E-mail already exists.")
+    }
+  
+    await this.usersRepository.create({
       name,
       email,
       password_hash,
-    },
-  })
+    })
+  }
+
 }
