1d57f6ccab1137f92ce0ef06871db1babf0d85b1
Paulo Santana <paulosantana.sa@gmail.com>
2024-06-16 01:54:00 -0300

    chore: Improve error handling in register controller and app.ts

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { UsersRepository } from "@/repositories/users-repository"
 import { hash } from "bcryptjs"
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
+import { User } from "@prisma/client"
 
 interface registerUseCaseRequest {
   name: string
@@ -8,23 +9,31 @@ interface registerUseCaseRequest {
   password: string
 }
 
-export class RegisterUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+interface RegisterUseCaseResponse {
+  user: User
+}
 
-  async execute({name, email, password}: registerUseCaseRequest) {
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute({ name, email, password }: registerUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
-  
+
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
-  
-    if(userWithSameEmail) {
+
+    if (userWithSameEmail) {
       throw new UserAlreadyExistsError()
     }
-  
-    await this.usersRepository.create({
+
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 
 }
