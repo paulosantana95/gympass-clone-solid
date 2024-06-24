9baccea6434ac48f4d2fa084303ebc3f85ae4b61
Paulo Santana <paulosantana.sa@gmail.com>
2024-06-23 23:37:00 -0300

    feat: Rename createdAt to created_at in CheckIn model and database table

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import { Prisma, User } from "@prisma/client";
 import { UsersRepository } from "../users-repository"
+import { randomUUID } from "node:crypto";
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
@@ -27,7 +28,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -39,19 +40,3 @@ export class InMemoryUsersRepository implements UsersRepository {
     return user
   }
 }
-
-// const registerUseCase = new RegisterUseCase({
-//   async findByEmail(email) {
-//     return null
-//   },
-
-//   async create(data) {
-//     return {
-//       id: 'user-1',
-//       name: data.name,
-//       email: data.email,
-//       password_hash: data.password_hash,
-//       created_at: new Date(),
-//     }
-//   }
-// })
