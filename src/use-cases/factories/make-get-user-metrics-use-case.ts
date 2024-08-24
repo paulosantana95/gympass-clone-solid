19fb87b5c9656a60d3aa31bdd280a15ace53d441
Paulo Santana <paulosantana.sa@gmail.com>
2024-08-24 15:05:35 -0300

    feat: create factories

---
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -0,0 +1,9 @@
+import { GetUserMetricsUseCaseCase } from "../get-user-metrics"
+import { PrismaCheckInsRepository } from "@/repositories/prisma/prisma-check-ins-repository"
+
+export function makeGetUserMetricsUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const useCase = new GetUserMetricsUseCaseCase(checkInsRepository)
+
+  return useCase
+}
